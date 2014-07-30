6f1ca22eb2787bb03bfd011f7c2d76bde1cea38f
Carl LeBlond <carl@easyochem.com>
2014-07-30 08:56:54 -0400

    Update template_update.php

--- template_update.php
+++ template_update.php
@@ -26,6 +26,8 @@ require_once(dirname(__FILE__) . '/../../../config.php');
 require_login(0, false);
 global $OUTPUT;
 $numofstereo = required_param('numofstereo', PARAM_TEXT);
-$easyonewmanbuildstring = file_get_contents('edit_fischer'.$numofstereo.'.html').file_get_contents('fischer_dragable.html');
+$temp = file_get_contents('fischer_dragable.html');
+$temp = str_replace("moodleroot", $CFG->wwwroot, $temp);
+$easyonewmanbuildstring = file_get_contents('edit_fischer'.$numofstereo.'.html').$temp;
 echo $easyonewmanbuildstring;
 
